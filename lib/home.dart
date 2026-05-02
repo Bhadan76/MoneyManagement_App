83c62decf8a3de3c08526191cb7abbdb2cdc161e
Bhadan <bhadanpaul@gmail.com>
2026-05-02 23:26:26 +0600

    home page create

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
+  late TabController _tabController;
+  @override
+  void initState() {
+
+    // TODO: implement initState
+    super.initState();
+    _tabController=TabController(length: 2, vsync: this);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Money Managemet App'),
+        title: Text('Money Managemet ',style: TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),),
+        actions: [
+          Icon(Icons.search,),
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'Income',icon: Icon(Icons.arrow_upward),),
+            Tab(text: 'Expense',icon: Icon(Icons.arrow_downward),),
+          ],
+        ),
+
+
+
       ),
     );
   }
